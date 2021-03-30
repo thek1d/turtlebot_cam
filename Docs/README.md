b67a50631372ac23fba7e3e73e7884b45db5e307
thek1d <oezguer.guemueslue@gmail.com>
2021-03-30 21:26:57 +0200

    added camera module

---
+++ Docs/README.md
@@ -0,0 +1,4 @@
+## Lauching custom world with waffle
+- add turtlebot3_cam_world.launch to /turtlebot3_gazebo/launch folder
+- specifiy robot model, export TURTLEBOT3_MODEL=waffle
+- roslaunch turtlebot3_gazebo turtlebot3_cam_world.launch  
---
+++ src/camera.py
@@ -0,0 +1,29 @@
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+def image_cb(ros_img, brigde):
+    try:
+        cv_image = brigde.imgmsg_to_cv2(ros_img, desired_encoding='bgr8')
+        
+    except CvBridgeError as e:
+        print(e)
+    show_img(cv_image)
+
+def show_img(cv_image):
+    cv2.imshow('waffle_stream', cv_image)
+    cv2.waitKey(10)
+
+def main():
+    rospy.init_node('cam_node', anonymous=True)
+    brigde = CvBridge()
+    rospy.Subscriber(name='/camera/rgb/image_raw', data_class=Image, callback=image_cb, \
+                     callback_args=brigde, queue_size=1)
+
+if __name__=='__main__':
+    main()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
